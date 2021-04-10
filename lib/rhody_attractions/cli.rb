256e644581893af089d773b81d29d97673b940d4
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-10 18:48:09 +0000

    modified cli class

--- lib/rhody_attractions/cli.rb
+++ lib/rhody_attractions/cli.rb
@@ -1,36 +1,68 @@
 class RhodyAttractions::CLI
   
   def greeting
-    puts "Getting rhody attractions"
+    puts "", "Welcome to RhodyAttractions", ""
+    # sleep 0.5
+    # this
     RhodyAttractions::Scraper.new.make_page
     list_places
+  end
+  
+  def this
+    3.times do 
+      x = "loading attractions."
+      y = x.length
+      while x.length < y + 3
+        print "\r#{x}"
+        sleep 0.5
+        print "\r#{" " * x.length}"
+        sleep 0.5
+        x = x + "."
+      end
+    end
+  end
+  
+  def loading
+    3.times do
+      print "\rloading."
+      sleep 0.5
+      print "\r        "
+      sleep 0.5
+      print "\rloading.."
+      sleep 0.5
+      print "\r         "
+      sleep 0.5
+      print "\rloading..."
+      sleep 0.5
+      print "\r          "
+      sleep 0.5
+    end
   end
     
   def list_places
     RhodyAttractions::Attraction.all.each.with_index(1) do |a, i|
       puts "#{i}. #{a.name}"
     end
-    pick_a_place
-  end
-  
-  def pick_a_place
-    puts ""
-    print "Pick a place: "
-    x = gets.strip.to_i
-    puts ""
-    a = RhodyAttractions::Attraction.find_by_index(x)
-    places_nearby(a)
+    select_attraction
   end
     
-  def function(x)
+  def select_attraction
+    puts "select attraction: "
+    a_index = gets.strip.to_i
+    a = RhodyAttractions::Attraction.find_by_index(a_index)
+    more_info(a)
+  end
+  
+  def more_info(x)
+    puts x.name
+    puts x.short_description.strip
+    puts x.town
+    puts x.url
+    places_nearby(x)
   end
   
   def places_nearby(x)
-    puts "Three closest attractions:"
     y = x.places_nearby.map {|a| a.text.strip.split("\n")}
-    y.each.with_index(1) do |a, i|
-      puts "#{i}. #{a[0]} - #{a[1]} from #{x.name}"
-    end
-    pick_a_place
+    y.each {|a| puts "#{a[0]} - #{a[1]}"}
   end
 end
