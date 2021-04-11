2de486bbc9342d068b34ecb4fbbb78ec484e9d76
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-11 20:51:57 +0000

    cli class  updated

--- lib/rhody_attractions/cli.rb
+++ lib/rhody_attractions/cli.rb
@@ -1,102 +1,34 @@
 class RhodyAttractions::CLI
   
-  def greeting
-    puts "", "Welcome to RhodyAttractions", ""
+  def start
+    greeting
     RhodyAttractions::Scraper.new.make_page
-    # binding.pry
-    list_towns
-    # list_places
-  end
-  
-  def this
-    3.times do 
-      x = "loading attractions."
-      y = x.length
-      while x.length < y + 3
-        print "\r#{x}"
-        sleep 0.5
-        print "\r#{" " * x.length}"
-        sleep 0.5
-        x = x + "."
-      end
-    end
-  end
-  
-  def list_towns
-    RhodyAttractions::Attraction.towns.each.with_index(1) do |a, i|
-      puts "#{i}. #{a}"
-    end
-    print "Enter a number: "
-    x = gets.strip.to_i
-    y = RhodyAttractions::Attraction.towns[x-1]
-    RhodyAttractions::Attraction.attractions_by_town(y).each do |a|
-      puts a.name
-    end
-  end
-  
-  def attraction_by_town(town)
-     RhodyAttractions::Attraction.all.each do |a|
-       if a.town == town
-         puts a.name
-       end
-     end
-  end
-    
-  def list_places
-    RhodyAttractions::Attraction.all.each.with_index(1) do |a, i|
-      puts "#{i}. #{a.name}"
-    end
+    list_attractions
     select_attraction
   end
     
-  def select_attraction
-    puts "select attraction: "
-    a_index = gets.strip.to_i
-    a = RhodyAttractions::Attraction.find_by_index(a_index)
-    short_summary(a)
+  def greeting
+    puts "Howdy!"
   end
-  
-  def short_summary(x)
-    puts "#{"-" * 20}#{x.name}#{"-" * 20}"
-    puts "Location: #{x.town}"
-    puts "Website: #{x.url}"
-    print_description(x.short_description)
     
-    puts ""
-    print "Would you like more info: "
-    a = gets.strip
-    if a == "y"
+  def list_attractions
+    RhodyAttractions::Attraction.all.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name} - #{a.town}"
+      a.short_description.scan(/(.{1,50})(?:\s|$)/m).each do |b|
+        puts "#{" " * i.to_s.length}  #{b[0].strip}"
+      end
       puts ""
-      detailed_summary(x)
     end
+    # x = gets.strip.to_i
+    # a = RhodyAttractions::Attraction.find_by_index(x)
+    # places_nearby(a)
   end
-  
-  def detailed_summary(x)
-    puts "#{"-" * 20}#{x.name}#{"-" * 20}"
-    puts "Location: #{x.street_address}, #{x.town}"
-    puts "Website: #{x.url}"
-    # puts "Short Description: #{x.short_description.strip}"
-    print_description(x.long_description)
-    places_nearby(x)
-  end
-  
-  def print_description(description)
-    puts ""
-    puts "Description: "
-    lines = description.split(/(.{1,50})(\s+|$)/)
-    lines.delete_if {|l| l == " " || l == "" || l == "\n"}
-    lines.each_with_index do |l, i|
-      lines[i] = "\t" + lines[i]
-    end
-    lines.each do |l|
-      puts l
-    end
+    
+  def select_attraction
   end
   
   def places_nearby(x)
-    puts ""
-    puts "Attractions nearby:"
     y = x.places_nearby.map {|a| a.text.strip.split("\n")}
-    y.each {|a| puts "\t#{a[0]} - #{a[1]} away"}
+    y.each {|a| puts "#{a[0]} - #{a[1]}"}
   end
 end
